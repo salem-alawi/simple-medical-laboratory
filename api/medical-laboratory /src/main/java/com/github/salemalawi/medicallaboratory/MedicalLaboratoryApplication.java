73ec4b13f1688411bb61ea0efc98a826c7b75f30
Salem Alawi <salemalowi@gmail.com>
2022-07-16 16:57:20 +0300

    init project  with doctor patient and section model

---
+++ api/medical-laboratory /src/main/java/com/github/salemalawi/medicallaboratory/MedicalLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package com.github.salemalawi.medicallaboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalLaboratoryApplication.class, args);
+	}
+
+}
